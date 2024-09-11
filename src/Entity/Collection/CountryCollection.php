7ebd1b64027bc2a575e465abfcb74b528a22fc57
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-09-11 11:53:22 +0200

    Ajout des classes des collection des pays                       ¶

---
+++ src/Entity/Collection/CountryCollection.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+
+class CountryCollection
+{
+    public static function findAll(): array{
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+SELECT * FROM country ORDER BY name
+SQL);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
---
+++ src/Entity/Country.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class Country
+{
+    private int $id;
+    private string $code;
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
