fff7da7af3173d32814393d4612ab8899b2a2e5f
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-09-11 11:41:55 +0200

    Test du fonctionnement des données de session

---
+++ public/demo/delete.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Html\AppWebPage;
+use Service\Session;
+
+Session::start();
+
+$webPage = new AppWebPage('Effacement des données de session');
+
+if (isset($_SESSION['MA_DONNEE_DE_SESSION'])) {
+    unset($_SESSION['MA_DONNEE_DE_SESSION']);
+    $webPage->appendContent('<h2>Donnée en session effacée</h2>');
+} else {
+    $webPage->appendContent('<h2>Pas de donnée en session</h2>');
+}
+
+echo $webPage->toHTML();
