6fad0232b6af542028e7aac3d6c1c40da8206c8d
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-09-18 11:29:09 +0200

    Ajout de la méthode setSelectedCodeFromSession et ajout des methode dans le constructeur directement

--- src/Html/Form/SessionManagedCountrySelect.php
+++ src/Html/Form/SessionManagedCountrySelect.php
@@ -9,9 +9,18 @@ class SessionManagedCountrySelect extends CountrySelect
     public function __construct(string $name, string $firstOption, string $defaultCode){
         parent::__construct($name, $firstOption, $defaultCode);
         Session::start();
+        $this->setSelectedCodeFromSession();
+        $this->saveSelectedCodeIntoSession();
     }
 
     public function saveSelectedCodeIntoSession(): void{
         $_SESSION[$this->getName()] = $this->getSelectedCode();
+    }
+
+    public function setSelectedCodeFromSession(): void
+    {
+        if (isset($_SESSION[$this->getName()]) && '' !== $_SESSION[$this->getName()]) {
+            $this->setSelectedCode($_SESSION[$this->getName()]);
+        }
     }
 }
