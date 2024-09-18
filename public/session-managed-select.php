9b3823c349550839676c1b6be4fcb6430cbf49d2
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-09-18 10:45:09 +0200

    Copie du fichier select.php

---
+++ public/session-managed-select.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Html\AppWebPage;
+use Html\Form\SessionManagedCountrySelect;
+
+$webPage = new AppWebPage('Country selector');
+
+$select = new SessionManagedCountrySelect('country', 'Pays', 'fr');
+
+$select->setSelectedCodeFromRequest();
+
+$webPage->appendContent(
+    <<<HTML
+    <form class="country-selector">
+        {$select->toHtml()}
+        <input type="submit" value="Choisir">
+    </form>
+    HTML
+);
+
+echo $webPage->toHTML();
+
