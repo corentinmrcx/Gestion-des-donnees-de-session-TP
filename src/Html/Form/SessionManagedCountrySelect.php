6cd9b89f65f3733ab3a91a3cc4126cfc1357de96
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-09-18 10:44:45 +0200

    Creation de la classe et démarrage de la session

---
+++ src/Html/Form/SessionManagedCountrySelect.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Html\Form;
+use Service\Session;
+
+class SessionManagedCountrySelect extends CountrySelect
+{
+    public function __construct(string $name, string $firstOption, string $defaultCode){
+        parent::__construct($name, $firstOption, $defaultCode);
+        Session::start();
+    }
+}
